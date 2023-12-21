7dd2994d6d2c049143a03b8d9a959ac02ab782a8
grot4486 <115872950+grot4486@users.noreply.github.com>
2023-12-21 11:43:25 -0500

    Add files via upload

---
+++ arif.py
@@ -0,0 +1,7 @@
+﻿from math import *
+
+
+alpha = int(input("Необходимо ввести угол в радианах: "))
+a1 = cos(alpha) + sin(alpha) + cos(3 * alpha) + sin(3 * alpha)
+a2 = 2 * (2 ** 0.5) * cos(alpha) * sin((pi / 4) + 2 * alpha)
+print(a1, a2)
