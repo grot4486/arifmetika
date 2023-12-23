3f18fe6a25c9d50311cce94b8afb25ac087d97c3
grot4486 <115872950+grot4486@users.noreply.github.com>
2023-12-23 10:23:28 +0700

    Update arif.py

--- arif.py
+++ arif.py
@@ -1,7 +1,7 @@
-﻿from math import *
+﻿from math import * # Импорт библиотеки math
 
 
-alpha = int(input("Необходимо ввести угол в радианах: "))
-a1 = cos(alpha) + sin(alpha) + cos(3 * alpha) + sin(3 * alpha)
-a2 = 2 * (2 ** 0.5) * cos(alpha) * sin((pi / 4) + 2 * alpha)
-print(a1, a2)
+alpha = int(input("Необходимо ввести угол в радианах: ")) # Ввод угла
+a1 = cos(alpha) + sin(alpha) + cos(3 * alpha) + sin(3 * alpha) # Вычисление значений функций
+a2 = 2 * (2 ** 0.5) * cos(alpha) * sin((pi / 4) + 2 * alpha) # Вычисление значений функций
+print(a1, a2) # Вывод результатов
